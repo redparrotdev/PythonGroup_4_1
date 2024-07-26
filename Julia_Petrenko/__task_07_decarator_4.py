935ce3ac28b31fe1535d2da9965f765d931a8fbb
Julia <JuliaVPetrenko@gmail.com>
2024-07-27 00:16:32 +0300

    add one more step to decarator

--- Julia_Petrenko/__task_07_decarator_4.py
+++ Julia_Petrenko/__task_07_decarator_4.py
@@ -1,17 +1,18 @@
-def repeat(func):
-    def wrapper(num, name="Bob"):
-        for i in range(num):
-            func(name)
+def repeat(num):
+    def wrapper(func):
+        def inner(name="Bob"):
+            for i in range(num):
+                func(name)
+
+        return inner
+
     return wrapper
 
 
-@repeat
+@repeat(4)
 def say_hello_to(name="Bob"):
     print(f"Hello, {name}")
 
 
-say_hello_to(4, "Kriss")
-say_hello_to(4)
+say_hello_to("Kriss")
 
-# say_hello_to = repeat(say_hello_to)
-# say_hello_to(4, "Bill")
